72f3a4c56bcbc7c286aa9d727f97800862ad1a40
radu-ux <radu.uivari99@gmail.com>
2021-03-06 20:09:06 +0200

    Deleted Wordl-Happiness-Report (moved in Charts folder)

--- World-Happiness-Report/public/js/index.js
+++
@@ -1,9 +0,0 @@
-import { MultiLineChart } from './multi_linechart.js'
-
-function main() {
-    const multiLineChart = new MultiLineChart(['./data/2015.csv', './data/2016.csv', './data/2017.csv', './data/2018.csv', './data/2019.csv']);
-    multiLineChart.createChart();
-}
-
-main()
-
